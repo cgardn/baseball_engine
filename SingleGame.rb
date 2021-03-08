7b75e8ec4f0d9e220e4072f5217767b228ce6f97
cgardn <christopher.gardn@gmail.com>
2021-03-08 00:34:40 -0500

    second commit

--- SingleGame.rb
+++ SingleGame.rb
@@ -1,5 +1,8 @@
 class SingleGame
   # processes a single game, a row at a time
+  # input: raw Retrosheet season file: [year][teamcode].EV*, i.e. 1989ATL.EVN
+  # output: single file with list of playerIDs, and each offensive play
+  #         they made along with gameID
   def initialize
     @id = ''
     @info = {}
@@ -7,6 +10,11 @@ class SingleGame
     @play_sub = []
     @er = []
     @proc_playOrSub = Proc.new {|x| x == 'play' || x == 'sub'}
+
+    # just collects all offensive plays by player code (i.e. all at-bats)
+    # ignores subs for now, which includes pinch-hits and pinch-runs
+    # dumps the raw play data
+    @offensivePlays = {}
 
     # tracking who is currently playing in which position, for the purpose of
     #   assigning stats for plays on defense
@@ -33,7 +41,6 @@ class SingleGame
     #   total from all hits during game - so a single with 2 on is 3 bases 
     #   gained, 2 from runners and 1 from hitter)
     # - also includes bases stolen
-    @playerOffTotals = {}
 
     # start field 3: 1 for home, 0 for visiting
     # fielding positions are 1-9, 10 for DH, 11=pinch hit, 12=pinch run
@@ -52,6 +59,8 @@ class SingleGame
     when @proc_playOrSub
       # include data[0] because we'll need to know if play or sub later
       @play_sub.push(data)
+      puts data.to_s
+      gets.chomp
     when 'data'
       # these are actually earned runs
       if data[1] == 'er'
@@ -70,24 +79,20 @@ class SingleGame
     end
   end
 
-  def record_atbats
-    # for each play, record batting player and what they did
-    @play_sub.each do |row|
-    end
+  def process_play
+    # extracts whatever I'm targeting out of the raw play codes, associating
+    #   play actions with player codes through the roster
+    # start with just getting offensive stuff (check first char for hit)
+    #   and count bases gained (advanced runners)
+    # then add in defensive stuff (total num of plays sent to player, also
+    #   increment when that person got at out (i.e. it wasn't a hit)
+    return nil
   end
 
   def sub_player(newPlayer)
     # [playerID, player fullname, home/vis (1/0), batting order, defPosition]
     @currentOff[newPlayer[2]][newPlayer[3]] = newPlayer[0]
     @currentDef[newPlayer[2]][newPlayer[4]] = newPlayer[0]
-  end
-
-  def separate_player_data
-    return nil
-  end
-
-  def clear
-    initialize
   end
 
   def has_data?
