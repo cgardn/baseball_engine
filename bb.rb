4c7c66a58e48eae76a3ce443aeec296a08f07748
cgardn <christopher.gardn@gmail.com>
2021-03-29 01:04:10 -0400

    fix console usage notes

--- bb.rb
+++ bb.rb
@@ -1,3 +1,5 @@
+#!/bin/ruby 
+
 require './lib/IngestChadwick'
 require './lib/Measure'
 require './lib/Analyze'
@@ -6,82 +8,81 @@ require './lib/Test'
 require './lib/DBInterface'
 
 class BB
-  USAGE_STR = "usage:
-    ingest [TABLENAME] - process raw retrosheet files into DB. Requires name of
-                         table to save data into in database
-    measure [START, LENGTH] - get measurements on LENGTH records, from START
-    train [START, LENGTH, MODEL] - generate chosen model on measurements
-    test [START, LENGTH] - test with generated model. recommend using different
-                           range from the one used for 'measure'\n\n
-    ---
-    Each command should be run in order from top to bottom
-    *NOTE* 'test' uses old (and incorrect) methodology, so it's just going to
-           give you a lot of garbage at the moment. Working on it! :)
-  
-    You'll also need event, team, and roster files from retrosheet.org.
-    Check README for more info.
-  
-    Call #help on this object to see this message again."
 
   def initialize
     @db = DBInterface.new
-    
-    puts USAGE_STR
+    @ingest = IngestChadwick
+    @measure = nil
+    @train = nil
+    @test = nil
   end
 
   def ingest(tableName)
-    ing = IngestChadwick.new(@db, tableName) 
+    ing = @ingest.new(@db, tableName)
     ing.ingest_raw_data
   end
 
   def measure(startNum, endNum, tableName)
-    measure = MeasureGame.new(@db, tableName)
+    check_if_implemented(@measure)
+    measure = @measure.new(@db, tableName)
     measure.generate_measurements(startNum, endNum)
   end
 
-  def train
+  def train(startNum, endNum, tableName)
     # show available strategies, pick one and generate trained model
+    # ??
+    check_if_implemented(@train)
+    train = @train.new(@db, modelName)
+    train.train_model(startNum, endNum, tableName)
   end
 
-  def test
+  def test(num, modelName)
     # show trained models available for testing, pick one and do test
+    check_if_implemented(@test)
+    test = @test.new(@db, modelName)
+    test.test_model(num)
   end
-
-  def help
-    puts USAGE_STR
+  
+  def check_if_implemented(method)
+    if !method
+      puts "Not implemented"
+      exit
+    end
   end
 end
 
-if ARGV[0] == 'test'
-  bb = BB.new
-  bb.ingest("test", [1989,1989])
-end
 
-=begin
-usageStr = "usage:
-  ingest [TABLENAME] - process raw retrosheet files into DB. Requires name of
-                       table to save data into in database
-  measure [START, LENGTH] - get measurements on LENGTH records, from START
-  train [START, LENGTH, MODEL] - generate chosen model on measurements
-  test [START, LENGTH] - test with generated model. recommend using different
-                         range from the one used for 'measure'\n\n
-  ---
-  Each command should be run in order from top to bottom
-  *NOTE* 'test' uses old (and incorrect) methodology, so it's just going to
-         give you a lot of garbage at the moment. Working on it! :)
-
-  You'll also need event, team, and roster files from retrosheet.org.
-  Check README for more info.
-
-  Call #help on this object to see this message again."
+usageStr = "" \
+  "Baseball Engine - an interactive platform for predicting MLB game winners "\
+  "\nusage:\n"\
+  "ingest [TABLENAME] \n"\
+    "\tprocess raw retrosheet files into DB. TABLENAME is name of table to\n"\
+    "\tsave data into in database\n"\
+  "measure [START, LENGTH, TABLE] \n"\
+    "\tget measurements on LENGTH records, beginning from START and \n"\
+    "\trunning over LENGTH, stored in TABLE\n"\
+  "train [START, LENGTH, MODEL] \n"\
+    "\tgenerate named model on measurements\n"\
+  "test [START, LENGTH] \n"\
+    "\ttest with generated model. recommend using different range from the\n"\
+    "\tone used for 'measure'"\
+  "\n---\n\n"\
+  "Each command should be run in order from top to bottom.\n"\
+  "03/21: Not implemented/not finished:\n"\
+  "\t- measure\n"\
+  "\t- train\n"\
+  "\t- test\n"\
+  "You'll also need event, team, and roster files from retrosheet.org.\n"\
+  "Check README for more info.\n\n"
   
 if !ARGV[0]
   puts usageStr
   exit
 end
 
-case ARGV[0]
+bb = BB.new
 
+case ARGV[0]
   
 when 'ingest'
   if ARGV.length < 2 || ARGV[1].class != String
@@ -89,19 +90,19 @@ when 'ingest'
     exit
   end
   # parse raw retrosheet files into db
-  ing = Ingest.new(ARGV[1])
-  ing.ingest_raw_data
+  bb = BB.new
+  bb.ingest(ARGV[1])
 when 'measure'
   # generate actual numbers used as signals
   if ARGV.length < 3 
     puts usageStr
     exit
   end
-  measure = Measure.new
-  measure.generate_measurements(ARGV[1].to_i, ARGV[2].to_i)
-when 'analyze'
-  aModule = Analyze.new
-  aModule.analyze_measurements
+  bb = BB.new
+  bb.measure(ARGV[1].to_i, ARGV[2].to_i)
+when 'train'
+  bb = BB.new
+  bb.train(ARGV[1].to_i, ARGV[2].to_i, ARGV[3])
 when 'test'
   if ARGV.length < 3 
     puts usageStr
@@ -109,7 +110,9 @@ when 'test'
   end
   test = Test.new
   test.full_test(ARGV[1].to_i, ARGV[2].to_i)
+when 'systest'
+  bb = BB.new
+  bb.ingest("test", [1989,1989])
 else
   puts usageStr
 end
-=end
